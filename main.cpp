b19f5a210d683e30a5539b84bd7f35707c8aaa5a
Ayodeji Fashola <fashtop3@gmail.com>
2021-01-31 17:08:07 +0100

    add mosquito to path

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    ServerThread serverThread;
-    serverThread.start();
     MainWindow w;
     w.show();
     return a.exec();
